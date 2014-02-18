0c0294522d741a94a0a7c5a7a48afcb1d35ffc58
qdbyshm <18911150977@163.com>
2014-02-18 10:29:48 +0800

    memoryMaster
    memoryMaster

---
+++ MemoryMaster/MemoryMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryMaster
+//
+//  Created by 孙 浩明 on 14-2-18.
+//  Copyright (c) 2014年 孙 浩明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
